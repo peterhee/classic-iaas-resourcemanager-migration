698bb7258070e63111a306337e3167e99071bd25
colincole <colinco@microsoft.com>
2016-05-23 10:45:51 -0500

    AsmToArmMigrationApiTookit
    Validation and testing toolkit to ensure a customer’s virtual network
    and contained VMs can cleanly migrate from ASM (classic) to ARM using
    the new Azure ARM Migration API. The toolkit contains process steps and
    tools for validating and testing customer’s actual data to ensure a
    smooth migration to ARM.

---
+++ AsmToArmMigrationApiToolset/OtherScripts/CleanupLabArmSync.ps1
@@ -0,0 +1,16 @@
+﻿Select-AzureRmSubscription -SubscriptionId a97a235d-55f9-4382-856a-e38f8b5b6d31
+
+$rgs = Get-AzureRmResourceGroup | Where-Object {$_.ResourceGroupName.StartsWith("u") -and $_.ResourceGroupName.EndsWith("-Migrated")}
+foreach ($resource in $rgs)
+{
+    try
+    {
+        Write-Host "Attempting to remove resource group: $($resource.ResourceGroupName)."
+        Remove-AzureRmResourceGroup -Name $resource.ResourceGroupName -Force
+        Write-Host "Success removing the resource group: $($resource.NamResourceGroupNamee)."
+    }
+    catch
+    {
+        Write-Host "Error removing the resource group: $($resource.ResourceGroupName). Following exception was caught $($_.Exception.Message)" -ForegroundColor "Red"
+    }
+}
---
+++ AsmToArmMigrationApiToolset/OtherScripts/ShutdownLab.ps1
@@ -0,0 +1,23 @@
+﻿Select-AzureSubscription -SubscriptionId a97a235d-55f9-4382-856a-e38f8b5b6d31
+
+$services = Get-AzureService | Where-Object {$_.ServiceName.StartsWith("y")}
+foreach ($svc in $services)
+{
+    try
+    {
+        $vms = Get-AzureVM -ServiceName $svc.ServiceName 
+        foreach ($vm in $vms)
+        {
+            if ($vm.PowerState -ne "Stopped")
+            {
+                Write-Host "Stopping the vm: $($vm.Name)."
+                Stop-AzureVM -ServiceName $svc.ServiceName -Name $vm.Name -Force
+                Write-Host "Success stopping the vm: $($vm.Name)." -ForegroundColor "Green"
+            }
+        }
+    }
+    catch
+    {
+        Write-Host "Error stopping the vm: $($vm.Name). Following exception was caught $($_.Exception.Message)" -ForegroundColor "Red"
+    }
+}
