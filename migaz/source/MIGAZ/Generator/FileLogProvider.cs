da5ee1c986137a42bf76d1ac9def59a0e8ecf10c
tomholl <tomholl@microsoft.com>
2016-07-12 14:49:29 +1000

    Refactor template generation out of UI

---
+++ migaz/source/MIGAZ/Generator/FileLogProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIGAZ.Generator
+{
+    class FileLogProvider : ILogProvider
+    {
+        public FileLogProvider()
+        {
+
+        }
+
+        public void WriteLog(string function, string message)
+        {
+            string logfilepath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\MIGAZ-" + string.Format("{0:yyyyMMdd}", DateTime.Now) + ".log";
+            string text = DateTime.Now.ToString() + "   " + function + "  " + message + Environment.NewLine;
+            File.AppendAllText(logfilepath, text);
+        }
+    }
+}
